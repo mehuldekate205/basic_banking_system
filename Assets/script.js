1ae47cf631e4e8c9e31effc0470e467f51d2e4ae
Mehul Dekate <mehuldekate20@gmail.com>
2022-03-31 14:23:29 +0530

    First Commit

---
+++ Assets/script.js
@@ -0,0 +1,20 @@
+let myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText);
+
+function sendMoney(){
+   var enterName = document.getElementById("enterName").value;
+   var enterAmount = parseInt(document.getElementById("enterAmount").value);
+
+   if (enterAmount > 8000) {
+      alert("Insufficient Balance.")
+   } else {
+      var findUserBankAccount = enterName + "BankBalance";
+      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+      var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
+      document.getElementById("myAccountBalance").innerText = myAccountBalance
+      document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+      alert(`Successful Transaction !!  
+      $${enterAmount} is sent to ${enterName}`)
+   }
+}
+
+
